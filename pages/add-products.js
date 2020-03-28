3b8a697483457fa6348aa5c4b3436a6575720480
Jean Abayo <j33n>
2020-03-28 20:52:36 +0200

    add more products

--- pages/add-products.js
+++ pages/add-products.js
@@ -9,7 +9,22 @@ import styles from './styles/add-products.module.scss';
 const CreateShop = () => {
   const router = useRouter();
 
+  const initialProductState = {
+    photo: '',
+    name: '',
+    description: '',
+    price: '',
+    quantity: '',
+    grams: '',
+  };
+
   const [selectedFile, setSelectedFile] = useState(null);
+
+  const [products, setProducts] = useState([{
+    0: {
+      initialProductState,
+    },
+  }]);
 
   useEffect(() => {
     router.prefetch('/create-shop');
@@ -24,7 +39,6 @@ const CreateShop = () => {
     });
   };
 
-
   const handleUploadProduct = async (event) => {
     if (event.target.files && event.target.files.length> 0) {
       const file = event.target.files[0];
@@ -35,72 +49,78 @@ const CreateShop = () => {
     }
   };
 
+  const handleAddMore = () => {
+    setProducts([...products, {[products.length + 1]: initialProductState}]);
+  };
+
   return (
     <Layout>
       <div className="container-md">
         <h1 className={styles.title}>Now add products</h1>
-        <div className={styles.container}>
-          <div className="row">
-            <div className="col-sm">
-              <input type="file" className={styles.file} onChange={handleUploadProduct} />
-              <div className={styles.photo} onClick={() => {}}>
-                {selectedFile && <img className={styles.image} src={selectedFile} />}
-                <span className={styles.add}>Add Photo</span>
+        {products && products.length > 0 && products.map((product, index) => (
+          <div className={styles.container} key={index}>
+            <div className="row">
+              <div className="col-sm">
+                <input type="file" className={styles.file} onChange={handleUploadProduct} />
+                <div className={styles.photo} onClick={() => {}}>
+                  {selectedFile && <img className={styles.image} src={selectedFile} />}
+                  <span className={styles.add}>Add Photo</span>
+                </div>
               </div>
-            </div>
-            <div className="col-sm">
-              <div className="form-group">
-                <label htmlFor="productName">Product name</label>
-                <input
-                  type="text"
-                  className="form-control"
-                  id="productName"
-                  placeholder="Enter product name"
-                />
-              </div>
-              <div className="form-group">
-                <label htmlFor="productDesc">Product description</label>
-                <textarea
-                  className="form-control"
-                  id="productDesc"
-                  rows="4"
-                ></textarea>
-              </div>
-              <div className="form-group">
-                <label htmlFor="price">Price</label>
-                <input
-                  type="text"
-                  className="form-control"
-                  id="price"
-                  placeholder="Enter price"
-                />
-              </div>
-              <div className="form-group">
-                <label htmlFor="quantity">Quantity</label>
-                <div className={styles.grams}>
+              <div className="col-sm">
+                <div className="form-group">
+                  <label htmlFor="productName">Product name</label>
                   <input
-                    type="number"
+                    type="text"
+                    className="form-control"
+                    id="productName"
+                    placeholder="Enter product name"
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="productDesc">Product description</label>
+                  <textarea
+                    className="form-control"
+                    id="productDesc"
+                    rows="4"
+                  ></textarea>
+                </div>
+                <div className="form-group">
+                  <label htmlFor="price">Price</label>
+                  <input
+                    type="text"
                     className="form-control"
                     id="price"
+                    placeholder="Enter price"
                   />
-                  <select className="form-control" id="grams" name="grams">
-                    <option>select grams</option>
-                    <option>0 - 10</option>
-                    <option>10 - 50</option>
-                    <option>50 - 100</option>
-                    <option>100 - 200</option>
-                    <option>200 - 1000</option>
-                  </select>
+                </div>
+                <div className="form-group">
+                  <label htmlFor="quantity">Quantity</label>
+                  <div className={styles.grams}>
+                    <input
+                      type="number"
+                      className="form-control"
+                      id="price"
+                    />
+                    <select className="form-control" id="grams" name="grams">
+                      <option>select grams</option>
+                      <option>0 - 10</option>
+                      <option>10 - 50</option>
+                      <option>50 - 100</option>
+                      <option>100 - 200</option>
+                      <option>200 - 1000</option>
+                    </select>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
+        ))}
         <div className={styles.submit}>
           <button
             type="button"
             className="btn btn-lg btn-primary btn-block"
-            disabled
+            onClick={handleAddMore}
           >
               Add More
           </button>
