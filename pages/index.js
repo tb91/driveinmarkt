c1cc5e3e8cd0871f7b65add086f66c054f06c7e3
Kaluã Bentes <kaluanbentes@gmail.com>
2020-03-29 20:12:17 -0300

    refactor: implement index page

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,37 @@
-import React from 'react';
-import MapScreen from '../utilities/map_screen';
-import Layout from '../utilities/layout';
+import React from "react";
+import { v4 as uuidv4 } from "uuid";
+
+import MapScreen from "../utilities/map_screen";
+import Layout from "../utilities/layout";
+import StoreResultCard from "../components/StoreResultCard";
+
+import styles from "./index.module.css";
+
+const stores = new Array(20).fill(undefined).map((_, index) => ({
+  image:
+    "https://gd-amsterda.sfo2.cdn.digitaloceanspaces.com/2016/04/coffee-shop-1-e1572034438285.jpg",
+  name: `Store ${index}`,
+  address: "Selchower Str. 1-6, 12049 Berlin",
+  tags: ["Books", "Coffee", "Food"]
+}));
 
 const Home = () => (
   <Layout>
-    <div className="w-100">
-      <MapScreen
-        loadingElement={<div style={{height: 'calc(100vh - 56px)'}} />}
-        containerElement=
-          {<div style={{width: '100%', height: `calc(100vh - 56px)`}} />}
-        mapElement={<div style={{height: 'calc(100vh - 56px)'}} />}
-      />
+    <div className={styles.grid}>
+      <div className={styles.storeListContainer}>
+        {stores.map(store => (
+          <StoreResultCard key={uuidv4()} {...store} />
+        ))}
+      </div>
+      <div className={styles.mapContainer}>
+        <MapScreen
+          loadingElement={<div style={{ height: "calc(100vh - 56px)" }} />}
+          containerElement={
+            <div style={{ width: "100%", height: `calc(100vh - 56px)` }} />
+          }
+          mapElement={<div style={{ height: "calc(100vh - 56px)" }} />}
+        />
+      </div>
     </div>
   </Layout>
 );
